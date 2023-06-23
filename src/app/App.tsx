6d83b8a471458106dd9f94ff6da21cfb0512c2ad
unknown <fraumaria2011@yandex.ru>
2023-06-23 12:13:03 +0300

    file names changed

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import './style/app.css'
+import './App.css'
 import Box from '@mui/material/Box';
 import Drawer from '@mui/material/Drawer';
 import ScrollToTop from '../shared/scroll-top/scroll-top';
