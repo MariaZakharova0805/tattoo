f8a80d512fa861481d7c46dbfa95e2cba5770636
unknown <fraumaria2011@yandex.ru>
2023-07-05 13:54:32 +0300

    appointment tape fixed

--- src/pages/appointment/appointment.tsx
+++ src/pages/appointment/appointment.tsx
@@ -11,26 +11,21 @@ export const Appointment = () => {
   const formVisible = useContactsFormVisibility(state => state.formVisible)
 
   return (
-    <Container>
-      <div className={formVisible ? c.block : c.block_hidden}>
-        <div className={c.header}>
-          <Header_H2 src={h2} alt="Book an appointment" />
-          <TextRegular textAlign="center">SEND US A MESSAGE! <br />We want to help you with your tattoo or piercing design.</TextRegular>
-          <TextRegular textAlign="center" color="grey">We appreciate setting up appointments but walk ins are always welcome. Please contact us with any questions you may have.</TextRegular>
-        </div>
+    <section className={c.section}>
+      <Container>
+        <Header_H2 src={h2} alt="Book an appointment" />
+        <TextRegular textAlign="center">SEND US A MESSAGE! <br />We want to help you with your tattoo or piercing design.</TextRegular>
+        <TextRegular textAlign="center" color="grey">We appreciate setting up appointments but walk ins are always welcome. Please contact us with any questions you may have.</TextRegular>
         <div className={c.form}><InputForm /></div>
-        <div className={c.block_tape}><Tape text1="Contact Us" text2="Contact US" /></div>
-      </div>
-      <div className={formVisible ? c.block_hidden : c.block_finish}>
-        <Header_H3>Success</Header_H3>
-        <p className={c.block_finish_header}>Your application is accepted</p>
-        <TextRegular textAlign="center" color="grey">We will contact you within 2 hours.</TextRegular>
-      </div>
-      <div className={formVisible ? c.block_hidden : c.block_finish}>
-        <Header_H3>Success</Header_H3>
-        <p className={c.block_finish_header}>Your application is accepted</p>
-        <TextRegular textAlign="center" color="grey">We will contact you within 2 hours.</TextRegular>
-      </div>
-    </Container>
+      </Container>
+      <Tape text1="Contact Us" text2="Contact US" />
+      <Container>
+        <div className={formVisible ? c.block_hidden : c.block_finish}>
+          <Header_H3>Success</Header_H3>
+          <p className={c.block_finish_header}>Your application is accepted</p>
+          <TextRegular textAlign="center" color="grey">We will contact you within 2 hours.</TextRegular>
+        </div>
+      </Container>
+    </section>
   )
 }
