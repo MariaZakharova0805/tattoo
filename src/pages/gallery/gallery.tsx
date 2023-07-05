aad02952c614ebf8dbf38168efe2f12d0bd6e9de
unknown <fraumaria2011@yandex.ru>
2023-07-05 09:51:11 +0300

    container width fixed

--- src/pages/gallery/gallery.tsx
+++ src/pages/gallery/gallery.tsx
@@ -5,8 +5,6 @@ import { PageBottom } from "../../widgets/page-footer/page-footer"
 import { TapeBlack } from "../../widgets/tape/tape-black"
 import { useGallery } from '../../shared/store'
 import { GalleryItem } from '../../entities/gallery-item/gallery-item'
-import { ModalImg } from '../../widgets/modal-img'
-
 
 export const Gallery = () => {
   const images = useGallery(state => state.images)
@@ -14,7 +12,6 @@ export const Gallery = () => {
     <section className={c.section}>
       <div><Header_H2 src={h2} alt="artists" /></div>
       <div className={c.tape}><TapeBlack text1="Enjoy Us" text2="Enjoy Us" /></div>
-      <ModalImg />
       <div className={c.gallery}>
         {images.map(item => {
           return (
--- src/widgets/modal-img/modal-img.tsx
+++
@@ -1,9 +0,0 @@
-import {FC} from 'react'
-
-type ModalImgProps = {}
-
-export const ModalImg:FC<ModalImgProps> = ({}) => {
-  return (
-    <div>modal-img</div>
-  )
-}
